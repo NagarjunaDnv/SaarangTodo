edb7330e46f234c886e48f9cce9316dbe6baf1f2
NagarjunaDnv <49153495+NagarjunaDnv@users.noreply.github.com>
2019-05-14 17:33:21 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import {ScreenOrientation} from '@ionic-native/screen-orientation/ngx'
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,7 +12,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private screenorientation:ScreenOrientation
   ) {
     this.initializeApp();
   }
@@ -21,6 +22,19 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      if(cordova.platformId="ios"){
+        this.statusBar.overlaysWebView(false);
+        this.statusBar.backgroundColorByHexString('#B3FCFFFE')
+        this.statusBar.styleLightContent()
+        this.statusBar.show()
+        }
+        if(cordova.platformId="android"){
+          this.statusBar.overlaysWebView(true)
+          this.statusBar.backgroundColorByHexString('#B3FCFFFE')
+          this.statusBar.show()
+        }
+        this.screenorientation.lock(this.screenorientation.ORIENTATIONS.PORTRAIT)
     });
+
   }
 }
